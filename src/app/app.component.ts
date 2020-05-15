072c226ebe5caa13c378629114dad260f8e52036
stevenglasford <stevenglasford@gmail.com>
2020-05-15 10:55:32 -0500

    Import several packages into the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+//angularfire is used as the primary angular fire member
+//authproviders and authmethods for authentication
+//firebaselistobservables allow for fetching of chat messages
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireObject, AngularFireList, AngularFireDatabaseModule } from "angularfire2/database"; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatApp';
+  items: AngularFireList<any>;
+  name: any;
+  msgVal: string = '';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import {AngularFireModule} from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAFzq54hdDqrrzgYelrW4UsK9xwyXB0RAI",
+  authDomain: "studentdata-5330a.firebaseapp.com",
+  databaseURL: "https://studentdata-5330a.firebaseio.com",
+  projectId: "studentdata-5330a",
+  storageBucket: "studentdata-5330a.appspot.com",
+  messagingSenderId: "16213049281",
+  appId: "1:16213049281:web:c0b22ff4620a507b68b940"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import {AngularFireModule} from 'angularfire2';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
